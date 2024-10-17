e71c6cfcd346457aee31d2b9e368bcc254281dfe
wangxiyi2000 <wangxiyi_2000@163.com>
2024-10-17 17:36:17 +0800

    提交b站赵虚左老师课程的历程代码——《服务通信》code（debug完成）、《参数服务器》code（刚刚开始）

---
+++ demo03_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/xxx/demo03_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/xxx/catkin_ws2/devel;/home/xxx/catkin_ws/devel;/home/xxx/workspace/livox/devel;/opt/ros/melodic'.split(';') if '/home/xxx/catkin_ws2/devel;/home/xxx/catkin_ws/devel;/home/xxx/workspace/livox/devel;/opt/ros/melodic' != '' else []
---
+++ demo03_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/xxx/Study_Project_ROS/demo03_ws/build/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/xxx/Study_Project_ROS/demo03_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/xxx/Study_Project_ROS/demo03_ws/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/xxx/Study_Project_ROS/demo03_ws/src/plumbing_param_server:$ROS_PACKAGE_PATH"
